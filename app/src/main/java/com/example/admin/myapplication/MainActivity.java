f55d9d022b2787c6e19911b0019a2c8da24a0a6e
Rajesh <rajeshdeva23@gmail.com>
2019-01-22 12:34:27 +0530

    fine

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -9,10 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         callfun();
     }
 
     private void callfun() {
+        System.out.println("git");
     }
 }
